3e6ef03852a83bf0a214d27d77da2d1b3ff7daa6
Vishnurr2k01 <vishnurr2k01@gmail.com>
2022-06-10 20:28:29 +0530

    .

---
+++ src/pages/Signin.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Signin() {
+    return (
+        <div>
+            <form action="">
+                <label htmlFor="name"></label>
+                <input type="text" name="name" id="" />
+                <label htmlFor="email"></label>
+                <input type="text" name="email" id="" />
+                <label htmlFor="password"></label>
+                <input type="password" name="password" id="" />
+                <label htmlFor="apikey"></label>
+                <input type="text" name="apikey" id="" />
+                <label htmlFor="apikey"></label>
+                <input type="text" name="apikey" id="" />
+
+            </form>
+        </div>
+    )
+}
+
+export default Signin
