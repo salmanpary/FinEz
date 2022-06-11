4b625d47826e77cf19a072115828ea1ffb36ed0f
LisaVC <lisaviju@gmail.com>
2022-06-11 06:45:51 +0530

    scene

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -51,50 +51,57 @@ function Details() {
     return (
         <div>
 
-            <div className="box_3d m-10 flex justify-between lg:mx-52 bg-gray-100 p-6 rounded-lg items-center">
+            <div className="box_3d m-10 flex justify-between lg:mx-52 bg-gray-100 p-7 rounded-lg items-center">
                 <div className="flex ">
 
                     <div className="">
-                        <h3 className='text-2xl font-bold mb-2 text-blue-800'>Bluechips</h3>
+                        <h3 className='text-2xl font-bold mb-1 text-blue-800'>Bluechips</h3>
                         <h6 className='text-xs mb-2 text-gray-700'>Managed by Altcoin Gordan</h6>
+
+                        <h5 className='max-w-[40ch] text-sm font-bold m-2 ml-0 mt-4'>BTC</h5>
+
                         <div class="w-full bg-white-100 rounded-xl blue_3d">
                             <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-1 leading-none rounded-l-full"> 50%</div>
                         </div>
-                        <h5 className='max-w-[40ch] text-sm font-bold m-2 ml-0'>50% BTC AND 50% ETH</h5>
+                        <h5 className='max-w-[40ch] text-sm font-bold m-2 ml-0 mt-4'>ETH</h5>
+
+                        <div class="w-full bg-white-100 rounded-xl blue_3d">
+                            <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-1 leading-none rounded-l-full"> 50%</div>
+                        </div>
 
 
 
                     </div>
                 </div>
-                <div className="">
-                    <h4 className='text-xl'>24h Change</h4>
-                    <h4 className='text-xl text-green-500'>14.4%</h4>
+                <div className='m-4'>
+                    <h4 className='text-l'>24h Change</h4>
+                    <h4 className='text-xl text-green-500 font-bold'>14.4%</h4>
                 </div>
             </div>
-            <fieldset className='border-4 w-[30rem] ml-[15rem] mt-12 p-4'>
-                <legend className='ml-4'><h3>Popular</h3></legend>
-                <h4>We have over 50k investors for this basket</h4>
+            <fieldset className='border-4 w-[24rem] ml-[15rem] mt-12 p-4 border-blue-800 rounded-2xl'>
+                <legend className='ml-4 p-2 font-bold'><h3>Popular</h3></legend>
+                <h4 className='mb-2'>We have over 50k investors for this basket</h4>
             </fieldset>
             <div className="flex ml-[15rem] mt-12">
                 <div className="">
                     <h3 className='text-2xl font-semibold '>Overview</h3>
 
                     <h4 className='text-xl mt-4'>About the Basket</h4>
-                    <p className='max-w-[75ch] mt-8'>The scheme invests in the higher marketcap coins Bitcoin and Ethereum</p>
+                    <p className='max-w-[75ch] mt-5 ml-0'>The scheme invests in the higher market cap coins Bitcoin and Ethereum</p>
                 </div>
                 <div className="ml-32">
                     <h5 className='text-xs'>Minimum Investment amount</h5>
                     <h3 className='text-2xl mt-4 font-bold'> 20 USDT</h3>
                     <h6 className='mt-4 text-sm'>Get free access forever</h6>
-                    <h5 className='text-sm font-bold'>See more benifits</h5>
-                    <button type="submit" disabled={disabled} className='mt-4 w-[10rem] py-2 border-2 border-green-400 text-green-400 hover:bg-green-400 hover:text-white rounded font-semibold text-xl' onClick={() => setShowModal(!modal)}> Invest Now</button> <br />
+                    <h5 className='text-sm font-bold'>See more benefits</h5>
+                    <button type="submit" disabled={disabled} className='mt-4 w-[10rem] py-2 blue_3d text-l text-white' onClick={() => setShowModal(!modal)}> Invest Now</button> <br />
                     {/* <button type="submit" className='mt-4 w-[10rem] py-2 border-2'>Add to Watchlist</button> <br /> */}
-                    <button type="submit" disabled={disabled} className='mt-4 w-[10rem] py-2 border-2 border-red-400 text-red-400 hover:bg-red-400 hover:text-white font-semibold rounded text-xl' onClick={sell}>sell</button>
+                    <button type="submit" disabled={disabled} className='mt-4 w-[10rem] py-2 red_3d text-l text-white' onClick={sell}>Sell</button>
                 </div>
                 {modal ? <Modal setShowModal={setShowModal} /> : <></>}
             </div>
             <div className="ml-[15rem] mt-8">
-                <h3 className='text-xl'>Weights</h3>
+                <h3 className='text-2xl font-semibold'>Weights</h3>
                 <div
                     ref={ref}
                     // here we use parent container size to resize chart depends on it 
