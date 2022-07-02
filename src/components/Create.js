b7dd17709b54a75a0c9a5b4c2802e2d4b58d2cff
LisaVC <lisaviju@gmail.com>
2022-07-02 18:49:55 +0530

    bg fixed

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,34 +1,33 @@
 import React from "react";
-import illa from '../images/illa.jpg'
-import create from '../images/create.jpg'
+import create from '../images/create.png'
 import { Link } from 'react-router-dom'
 
 const Create = () => {
     return (
         <>
 
-            <div className="flex">
+            <div className="flex mt-10">
 
 
-                <img src={create} className="img_create "></img>
+                <img src={create} className="img_create"></img>
                 <div className="info ml-20  my-10 mx-8 ">
-                    <div className="heading1 text-5xl font-bold m-3 ">
+                    <div className="heading1 text-5xl font-bold m-3 text-white">
                         Got a <span className="text-blue-800 font-bold text-6xl"> GREAT</span>
                     </div>
-                    <div className="heading1 text-5xl font-bold m-3">
+                    <div className="heading1 text-5xl font-bold m-3 text-white">
                         investment idea ?
                     </div>
-                    <div className="desc text-gray-500 m-3">
+                    <div className="desc text-gray-300 m-3">
                         Create or customize a basket to save it here
                     </div>
-                    <div className="desc text-gray-500 m-3">
+                    <div className="desc text-gray-300 m-3">
                         and track them
                     </div>
                     <div className="button text-2xl rounded-md m-3">
-<Link to="/createbasket">
-                        <button className="bg-blue-800 px-8 py-2 mt-4 rounded-xl text-white box_3d">
-                            Create Basket
-                        </button>
+                        <Link to="/createbasket">
+                            <button className="bg-blue-800 px-8 py-2 mt-4 rounded-xl text-white box_3d">
+                                Create Basket
+                            </button>
                         </Link>
                     </div>
 
